49fb9d1b3deabeecd9781ad2807bdae64c69e712
Nelson Pecora <nelson@yoshokatana.com>
2017-01-09 14:33:21 -0500

    call it randomString, with an optional prefix

---
+++ helpers/strings/randomString.js
@@ -0,0 +1,16 @@
+const _ = require('lodash'),
+  randomstring = require('randomstring');
+
+/**
+ * generatea random string
+ * e.g. `greatest-hit-` → `greatest-hit-noctz56h`
+ * @param  {string} [prefix]
+ * @return {string}
+ */
+module.exports = function (prefix) {
+  if (!_.isString(prefix)) {
+    prefix = '';
+  }
+
+  return prefix + randomstring.generate(8);
+};
---
+++ helpers/strings/randomString.test.js
@@ -0,0 +1,15 @@
+const name = getName(__filename);
+
+describe(name, function () {
+  it('returns random 8 character string', function () {
+    expect(hbs.compile('{{ randomString }}')().length).to.equal(8);
+  });
+
+  it('adds a prefix', function () {
+    expect(hbs.compile('{{ randomString a }}')({a: 'abc-'}).length).to.equal(12);
+  });
+
+  it('disregards non-string prefixes', function () {
+    expect(hbs.compile('{{ randomString a }}')({a: {b: 'c'}}).length).to.equal(8);
+  });
+});
