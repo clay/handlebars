08c1cece188b7fb720c1762f6438d53c51d38c90
Nelson Pecora <nelson@yoshokatana.com>
2017-01-09 12:12:56 -0500

    indexOf

---
+++ helpers/misc/indexOf.test.js
@@ -0,0 +1,20 @@
+const name = getName(__filename),
+  tpl = hbs.compile('{{ indexOf a b }}');
+
+describe(name, function () {
+  it('throws an error if no value passed in', function () {
+    const result = function () {
+      return tpl({a: null, b: null});
+    };
+
+    expect(result).to.throw(Error);
+  });
+
+  it('finds indexes in strings', function () {
+    expect(tpl({a: 'foo', b: 'f'})).to.equal('0');
+  });
+
+  it('finds indexes in arrays', function () {
+    expect(tpl({a: ['f', 'o', 'o'], b: 'f'})).to.equal('0');
+  });
+});
