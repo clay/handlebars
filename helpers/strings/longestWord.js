4e6711d7ec2b7f7ed42410536e30d2b81ef14b69
Nelson Pecora <nelson@yoshokatana.com>
2017-01-09 17:45:32 -0500

    longestWord

---
+++ helpers/strings/longestWord.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+
+/**
+ * returns the number of characters in the longest word of
+ * a string. Punctuation is NOT ignored.
+ * @param  {string} str
+ * @return {number}
+ */
+module.exports = function (str) {
+  if (!_.isString(str)) {
+    throw new Error('longestWord requires a string argument!');
+  }
+
+  return _.words(str).reduce(function (prev, curr) {
+    if (curr.length > prev.length) {
+      return curr;
+    } else {
+      return prev;
+    }
+  }, '').length;
+};
