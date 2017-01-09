afd7c808282f14f3f68dd4b3d219e64faea56f88
Nelson Pecora <nelson@yoshokatana.com>
2017-01-09 12:39:24 -0500

    toK

---
+++ helpers/numbers/toK.js
@@ -0,0 +1,24 @@
+/**
+ * convert number to shorthand
+ * e.g. 1000 → 1k
+ * @param  {number|string} x
+ * @return {string}
+ */
+module.exports = function (x) {
+  const currentNumber = parseInt(x, 10); // always specify radix with arbitrary input
+
+  if (isNaN(currentNumber)) {
+    return x; // fail gracefully
+  } else if (currentNumber > 999) {
+    const kResult = (currentNumber / 1000).toFixed(1);
+
+    if (parseFloat(kResult) === parseInt(kResult, 10)) {
+      return parseInt(kResult, 10) + 'k';
+    } else {
+      return kResult + 'k';
+    }
+  } else {
+    // number is less than 1000, so no conversion necessary
+    return x;
+  }
+};
---
+++ helpers/numbers/toK.test.js
@@ -0,0 +1,21 @@
+const name = getName(__filename),
+  tpl = hbs.compile('{{ toK a }}');
+
+describe(name, function () {
+  it('fails gracefully', function () {
+    expect(tpl({a: 'foo'})).to.equal('foo');
+  });
+
+  it('passes through sub-1000 numbers', function () {
+    expect(tpl({a: 999})).to.equal('999');
+  });
+
+  it('handles numbers and strings', function () {
+    expect(tpl({a: '1000'})).to.equal('1k');
+    expect(tpl({a: 1000})).to.equal('1k');
+  });
+
+  it('converts large numbers', function () {
+    expect(tpl({a: 100000})).to.equal('100k');
+  });
+});
