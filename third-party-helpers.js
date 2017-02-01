f99205875ebaaf7a57c3c6dc2b5fe41bfe329ec9
Nelson Pecora <nelson@yoshokatana.com>
2017-02-01 16:49:06 -0500

    add moment helper

--- third-party-helpers.js
+++ third-party-helpers.js
@@ -1,6 +1,7 @@
 'use strict';
 // a unified collection of third party helpers, used on both the client and the server
-const yaml = require('helper-yaml');
+const yaml = require('helper-yaml'),
+  date = require('helper-date');
 
 /**
  * pass an environment in
@@ -8,4 +9,5 @@ const yaml = require('helper-yaml');
  */
 module.exports = function (env) {
   env.registerHelper('yaml', yaml.sync);
+  env.registerHelper('moment', date);
 };
