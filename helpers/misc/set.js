513508fb470d03e8bec551cd684bb9c19b9d2fa3
David Andrew Frey <davidandrewfrey@gmail.com>
2017-02-21 14:55:06 -0500

    helper for storing variables across contexts

--- helpers/misc/set.js
+++ helpers/misc/set.js
@@ -2,13 +2,21 @@
 const _ = require('lodash');
 
 /**
- * set data into current context
+ * set data into current context or other optional context/object
  * _note:_ doesn't return anything
+ * @param  {object} [obj] context or object for storing data beyond current context
  * @param  {string} key `_.set()` key/path
  * @param  {*} val value to set
  */
-module.exports = function (key, val) {
-  _.set(this, key, val);
+module.exports = function (obj, key, val) {
+
+  if (_.isString(obj)) {
+    val = key;
+    key = obj;
+    obj = this;
+  }
+
+  _.set(obj, key, val);
 };
 
 module.exports.example = {
