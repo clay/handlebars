70f3575f81b956dc5454964401786664866dd5c0
Zoe Chodosh <zonika@users.noreply.github.com>
2017-07-20 13:13:03 -0400

    DateMinimal helper (#45)
    * add argument to articleDate that will show only the date, without time, when applicable
    
    * instead of using a flag, make a new helper called dateMinimal that only shows dates without time

--- README.md
+++ README.md
@@ -122,6 +122,7 @@ Currently **54 helpers** in **10 categories**:
 ### time
 
 * [**articleDate**](https://github.com/nymag/nymag-handlebars#articledate--code--tests-) ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/articleDate.js) | [tests](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/articleDate.test.js) )
+* [**dateMinimal**](https://github.com/nymag/nymag-handlebars#dateminimal--code--tests-) ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/dateMinimal.js) | [tests](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/dateMinimal.test.js) )
 * [**formatLocalDate**](https://github.com/nymag/nymag-handlebars#formatlocaldate--code--tests-) ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/formatLocalDate.js) | [tests](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/formatLocalDate.test.js) )
 * [**moment**](https://github.com/nymag/nymag-handlebars#moment--code--tests-) ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/moment.js) | no tests )
 
@@ -1000,6 +1001,22 @@ generate article dates and times, based on a relative format
 ```hbs
 {{ articleDate "Fri, 13 Jan 2017 18:22:16 GMT" }}
 //=> "Yesterday at 6:22 p.m."
+```
+
+### dateMinimal ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/dateMinimal.js) | [tests](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/dateMinimal.test.js) )
+
+generate display date (without time), based on a relative format
+
+#### Params
+* `datetime` _(Date|string)_ for  `date-fns`  to parse
+
+**Returns** _(string)_ 
+
+#### Example
+
+```hbs
+{{ dateMinimal "Fri, 13 Jan 2017 18:22:16 GMT" }}
+//=> "Yesterday"
 ```
 
 ### formatLocalDate ( [code](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/formatLocalDate.js) | [tests](https://github.com/nymag/nymag-handlebars/blob/master/helpers/time/formatLocalDate.test.js) )
---
+++ helpers/time/dateMinimal.test.js
@@ -0,0 +1,108 @@
+'use strict';
+const name = getName(__filename),
+  timeOnly = /^\d+:\d+ [ap]\.m\./,
+  timeAgoSec = /1 second ago/,
+  timeAgoSecs = /([12]?\d|30) seconds ago/,
+  timeAgoMins = /([12]?\d|30) (min|mins) ago/,
+  yesterday = /^Yesterday/,
+  dateAtTime = /^\d+\/\d+\/\d+/,
+  tpl = hbs.compile('{{ dateMinimal a}}');
+
+describe(name, function () {
+  function addDays(date, days) {
+    var result = new Date(date);
+
+    result.setDate(date.getDate() + days);
+    return result;
+  }
+
+  function matchMinutes(date, minutes) {
+    var result = new Date(date);
+
+    result.setMinutes(date.getMinutes() + minutes);
+    return result;
+  }
+
+  function matchSeconds(date, seconds) {
+    var result = new Date(date);
+
+    result.setSeconds(date.getSeconds() + seconds);
+    return result;
+  }
+
+  it('should return empty string if given an invalid date', function () {
+    var result = tpl({a: [2015, 25, 35]});
+
+    expect(result).to.equal('');
+  });
+
+  it('should match the ocluded frame', function () {
+    var result = tpl({a: new Date()});
+
+    expect(result).to.match(timeAgoSecs);
+  });
+
+  it('should show a time ago in seconds if article is created 1 second ago', function () {
+    var date = new Date(),
+      result;
+
+    date = matchSeconds(date, -1);
+    result = tpl({a: date});
+    expect(result).to.match(timeAgoSec);
+  });
+
+  it('should show a time ago in seconds if article is created < 1 minute ago', function () {
+    var date = new Date(),
+      result;
+
+    date = matchSeconds(date, -30);
+    result = tpl({a: date});
+    expect(result).to.match(timeAgoSecs);
+  });
+
+  it('should show a time ago in minutes if article is created < 30 minutes ago', function () {
+    var date = new Date(),
+      result;
+
+    date = matchMinutes(date, -5);
+    result = tpl({a: date});
+    expect(result).to.match(timeAgoMins);
+  });
+
+  it('should match h:mm if article is created > 30 minutes ago', function () {
+    var date = new Date(),
+      result;
+
+    date = matchMinutes(date, -35);
+    result = tpl({a: date});
+    expect(result).to.match(timeOnly);
+  });
+
+  it('should match one day ago', function () {
+    var date = new Date(),
+      result;
+
+    date = addDays(date, -1);
+    result = tpl({a: date});
+    expect(result).to.match(yesterday);
+  });
+
+  it('should match two days ago', function () {
+    var date = new Date(),
+      result;
+
+    date = addDays(date, -2);
+    result = tpl({a: date});
+    expect(result).to.match(dateAtTime);
+  });
+
+  it('should match one week ago', function () {
+    var date = new Date(),
+      result;
+
+    date = addDays(date, -7);
+    result = tpl({a: date});
+    expect(result).to.match(dateAtTime);
+  });
+
+});
