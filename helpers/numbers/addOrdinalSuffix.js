a7b2fb6a9e5389508a759b0b32ea51b44db88cec
Terri Neal <terri.neal@nymag.com>
2017-08-07 11:24:23 -0400

    helper to add an ordinal suffix after a number

--- helpers/numbers/addOrdinalSuffix.js
+++ helpers/numbers/addOrdinalSuffix.js
@@ -6,11 +6,14 @@
  * @param  {number|string} i number to add ordinal after
  * @return {string}
  */
-
 module.exports = function (i) {
   const j = i % 10,
-        k = i % 100;
-        
+    k = i % 100;
+
+  // if no value is supplied return an empty string
+  if (i == '') {
+    return '';
+  }
   if (j == 1 && k != 11) {
     return i + 'st';
   }
